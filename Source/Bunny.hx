ab4095f636cab35a9db9d585c7566e0547949647
mrcdk <codecdakiller@gmail.com>
2014-05-21 04:05:22 +0200

    initial commit

---
+++ Source/Bunny.hx
@@ -0,0 +1,29 @@
+package;
+
+
+import flash.geom.Point;
+import openfl.display.Tilesheet;
+
+
+class Bunny {
+	
+	
+	public var alpha:Float;
+	public var position:Point;
+	public var rotation:Float;
+	public var scale:Float;
+	public var speedX:Float;
+	public var speedY:Float;
+	
+	public var tilesheet:Tilesheet;
+	
+	
+	public function new () {
+		
+		speedX = 0;
+		speedY = 0;
+		
+	}
+	
+	
+}
